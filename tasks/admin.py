91c5f2b01384fd96a681075ff30c3f654eee6250
benedito <benethowen@yahoo.com.br>
2022-05-04 22:48:12 -0300

    Adicionando o app task no admin; carragando a listagem na views(links e conteúdos).

--- tasks/admin.py
+++ tasks/admin.py
@@ -1,3 +1,5 @@
 from django.contrib import admin
+from.models import Task #importando para área do admin
 
 # Register your models here.
+admin.site.register(Task)
--- tasks/views.py
+++ tasks/views.py
@@ -1,18 +1,24 @@
 from http.client import HTTPResponse
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 
 from django.http import HttpResponse #import do pacote HTTPResponse
 
+from .models import Task # importa o model
 # Create your views here.
 
+def taskList(request):
+    tasks = Task.objects.all() # select de todos os resultados 
+    return render(request, 'tasks/list.html', {'tasks': tasks})
+
+def taskView(request, id):
+    task = get_object_or_404(Task, pk=id) # pega a chave primária (pk) com get
+    return render(request, 'tasks/task.html', {'task': task})
+    
 #teste da página
 def helloWorld(request):
     return HttpResponse("Hello Benedito, eu sou Django")
 
 # Gerando uma página na view
-
-def taskList(request):
-    return render(request, 'tasks/list.html')
 
 # Gerando uma págian com argumento
 def yourName(request, name):
